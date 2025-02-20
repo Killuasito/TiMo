10ed59f2a9bb77966e634bd7918cca0295db3460
Tiago <tififerreira@gmail.com>
2025-02-20 17:03:10 -0300

    spotify3

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -1,6 +1,6 @@
 export const spotifyConfig = {
   clientId: "cfcd496346374776861564414cc3656c",
-  redirectUri: "http://localhost:5173/spotify-callback",
+  redirectUri: "https://timo-six.vercel.app/spotify-callback",
   scopes: [
     "streaming",
     "user-read-email",
