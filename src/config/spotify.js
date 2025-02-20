dbeeb3c3c056447094480f715a6b276deb19c7c6
Tiago <tififerreira@gmail.com>
2025-02-20 16:42:21 -0300

    teste spotify 16

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -1,6 +1,6 @@
 export const spotifyConfig = {
   clientId: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-  redirectUri: "https://timo-six.vercel.app/",
+  redirectUri: "https://timo-six.vercel.app", // Changed to root URL
   scopes: [
     "streaming",
     "user-read-email",
