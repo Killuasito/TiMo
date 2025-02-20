8c200dafe432479392b14bec8fc252afa08d1524
Tiago <tififerreira@gmail.com>
2025-02-20 16:37:39 -0300

    teste spotify 14

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import { ThemeProvider } from "./context/ThemeContext";
 import ChatComponent from "./components/ChatComponent";
 import PetWrapper from "./components/PetWrapper";
-import SpotifyCallback from "./components/SpotifyCallback";
 
 function App() {
   return (
@@ -52,10 +51,6 @@ function App() {
                     <PetWrapper />
                   </div>
                 }
-              />
-              <Route
-                path="/auth/spotify/callback"
-                element={<SpotifyCallback />}
               />
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
